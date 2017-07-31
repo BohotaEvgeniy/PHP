ffd6f084ff655b00a1a45d25f2d60c6df556bc85
bohota <bohota81@gmail.com>
2017-08-01 00:51:57 +0300

    My project Week 5

---
+++ Lesson5/StoreTask_4/index.php
@@ -0,0 +1,103 @@
+<? include("functions.php"); ?>
+<? include("products.php"); ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <!-- Basic Page Needs
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta charset="utf-8">
+    <title>OkaY</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Mobile Specific Metas
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- FONT
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+    <!-- CSS
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/skeleton.css">
+    <link rel="stylesheet" href="css/normalize.css">
+
+
+</head>
+<body>
+
+<!-- Primary Page Layout
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="two columns">
+                <h1><a href="">Brand</a></h1>
+            </div>
+            <div class="ten columns">
+
+            </div>
+        </div>
+    </div>
+</header>
+<div class="container">
+    <div class="row">
+        <div class="three columns">
+            <aside>
+                <?php GetCategoriesTree($categories_data); ?>
+            </aside>
+        </div>
+        <div class="nine columns">
+            <? if($products) :?>
+                <? foreach ($products as $product) :?>
+                    <div class="one-third column" style="margin-top: 5%">
+                        <? if($product->visible):?>
+                            <span class="date"><? echo date('m.d.y',strtotime($product -> created)) ?></span>
+                            <div class="image"> Image </div>
+                            <div class="product_name">
+                                <a href="<? echo $product->url ?>"><? echo $product -> name ?></a>
+                            </div>
+                            <div class="product_price">
+                                Цена <? echo round($product->variants[0] -> price, 2) ?>
+                                <? echo $product->variants[0] -> sku ?>
+                            </div>
+                            <div class="product_variables">
+                                <? if (count($product -> variants) > 1): ?>
+                                    <select class="fix">
+                                        <? foreach ($product -> variants as $v) :?>
+                                            <option value="">
+                                                <? echo round($v -> price, 2); ?>
+                                                <? echo $v -> sku; ?>
+                                            </option>
+                                        <? endforeach; ?>
+                                    </select>
+                                <? endif; ?>
+                            </div>
+                            <div class="stock">
+                                Товар на складе <? echo $product->variants[0]->stock ?> шт
+                            </div>
+                        <? endif; ?>
+                    </div>
+                <? endforeach; ?>
+            <? endif;?>
+        </div>
+    </div>
+</div>
+<footer>
+    <div class="container">
+        <div class="row">
+           <p>Footer</p>
+        </div>
+    </div>
+</footer>
+
+<!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+</body>
+</html>
+
