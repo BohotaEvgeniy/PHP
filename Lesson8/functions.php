81cb66456cf70366a48bc0ab95b423abce54ef8a
Evil Genius <Bohota81@gmail.com>
2017-09-05 21:40:38 +0300

    forms were fixed

--- Lesson8/functions.php
+++ Lesson8/functions.php
@@ -1,41 +1,32 @@
 <?php
 session_start();
 $data = $_POST;
-if ( isset($data['action']) ) {
+if ( isset($data['action']) ) {  // Если кнопка нажата
 
     $errors = array();
-    if ( !preg_match('/^[a-zA_Z0-9-]{4,}/i',$data['userLogin']) && (trim($data['userLogin']) != '')){
+    if ( !preg_match('/^[a-zA_Z0-9-]{4,}/i',$data['userLogin']) && (trim($data['userLogin']) != '')){   // Если  не  соответствует Pregmatch(regular) логину и поле логин не пустое
         $errors[] = "Enter login.Login isn't correct.Enter please numbers or letters not less 4 symbols";
     }
 
-    if ( !preg_match('/^[a-zA_Z0-9]{4,}/i',$data['userLogin']) && ($data['userPassword'] != '') ){
+    if ( !preg_match('/^[a-zA_Z0-9]{4,}/i',$data['userLogin']) && ($data['userPassword'] != '') ){  // Если  не  соответствует Pregmatch(regular) пароль и поле пароль не пустое
         $errors[] = "Enter Password";
     }
 
-    if ( trim($data['userPassword2']) != $data['userPassword'] ){
+    if ( trim($data['userPassword2']) != $data['userPassword'] ){ // еще одна проверка на пароль
         $errors[] = "Enter Password repeat";
     }
 
-    if ( empty($errors) ) {
-        $files = 'files/users.txt';
-        $f = fopen($files, 'a+');
-        if(!stristr(file_get_contents($files), strtolower($data['userLogin']))){
-            $containUser = strtolower($data['userLogin']) . ' ||| ' . password_hash($data['userPassword'],PASSWORD_DEFAULT) . PHP_EOL;
-            file_put_contents($files,$containUser,FILE_APPEND | LOCK_EX);
-            while  (!feof($f)) {
-                $containStr = fgets($f);
-                $lengthStr = strlen($containStr);
-                $sliceStrHash = substr($containStr,-62);
-                $lengthSliceStrLogin = $lengthStr - 66;
-                $SliceStrLogin = substr($containStr,0,$lengthSliceStrLogin);
-            }
-            header('Location: http://php/Lesson8/task_2.php');
+    if ( empty($errors) ) {   // Если массив ошибок пуст,тогда
+        $files = 'files/users.txt'; // создаем файл в директории
+        $containStr .= strtolower($data['userLogin']) . '|||' . password_hash($data['userPassword'],PASSWORD_DEFAULT) . PHP_EOL; // готовая строка логин + пароль
+        if(!stristr(file_get_contents($files), $data['userLogin'])){ // Проверка на вхождение логина в строку(users.txt)
+            file_put_contents($files,$containStr,FILE_APPEND | LOCK_EX); // Вписываем логин + пароль
         } else {
             echo 'Enter other "Login".Login already exist!';
         }
 
     } else {
-        echo '<div>' . array_shift($errors) . '</div>';
+        echo '<div>' . array_shift($errors) . '</div>'; // вывод ошибки
     }
 }
 
@@ -53,25 +44,23 @@ if ( isset($data['submit']) ) {
     }
 
     if ( empty($errors) ) {
-        $f = fopen('files/users.txt', 'a+');
         $password = $data['userPassword'];
-        if(stristr(file_get_contents('files/users.txt'), strtolower($data['userLogin']))){
-            $containUser = strtolower($data['userLogin']) . ' ||| ' . password_hash($data['userPassword'],PASSWORD_DEFAULT) . PHP_EOL;
-            while  (!feof($f)) {
-                $containStr = fgets($f);
-                $lengthStr = strlen($containStr);
-                $sliceStrHash = substr($containStr,-62);
-                $lengthSliceStrLogin = $lengthStr - 66;
-                $sliceStrLogin = substr($containStr,0,$lengthSliceStrLogin);
-                if (password_verify($password,$sliceStrHash)){
-                    echo 'Invalid password!';
-                } else {
-                    echo 'Password is invalid' . "\n";
-                    $_SESSION['name'] = $sliceStrLogin;
-                    echo $_SESSION['name'];
-                    exit;
+        if(stristr(file_get_contents('files/users.txt'), strtolower($data['userLogin']))){ // проверка на совпадение логина в строке
+            $file_handle = fopen('files/users.txt', "r"); // Считываем файл
+            while (!feof($file_handle)) {  // Пока файл не достигнет конца
+                $tmp = fgets($file_handle); // Считываем строку
+                if (stristr($tmp,$data['userLogin'])) { // еще проверка на совпадение логина в конкретной строке
+                   if (password_verify($password,trim(stristr($tmp,"$2y$10$")) )) { // проверка на правильность пароля пользователя
+                        echo "Welcome to home mr.";
+                        $_SESSION['name'] = $data['userLogin'];
+                        exit;
+                    } else {
+                       echo "Wrong password";
+                   }
                 }
             }
+            fclose($file_handle);
+
         } else {
             echo "You don't register'";
         }
