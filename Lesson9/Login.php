65b5f53c1b836bda6b5a7f6f9c7dee5d6f7ce886
Evil Genius <Bohota81@gmail.com>
2017-09-20 23:27:34 +0300

    unite PHP+HTML/fix login

--- Lesson9/Login.php
+++ Lesson9/Login.php
@@ -1,4 +1,54 @@
-<?php require_once 'functions.php'?>
+<?php include_once 'db.php'?>
+<?php session_start(); ?>
+<?php
+$data = $_POST;
+if ( isset($data['submit']) ) {
+
+    $errors = array();
+    if (!preg_match('/^[a-zA_Z0-9-]{4,}/i', $data['userLogin']) && (trim($data['userLogin']) != '')) {
+        $errors[] = "Enter login.Login isn't correct.Enter please numbers or letters not less 4 symbols";
+    }
+
+    if (!preg_match('/^[a-zA_Z0-9]{4,}/i', $data['userLogin']) && ($data['userPassword'] != '')) {
+        $errors[] = "Enter Password";
+    }
+
+    if (empty($errors)) {
+        $password = $data['userPassword']; //заносим пароль
+        $pathDb = includeDB(); // подключаемся к БД
+        $containStrLogin = strtolower($data['userLogin']); // Храним Логин
+        $selectIntoDbAllU = "SELECT * FROM `users`"; // Подготовка выборки
+        $selectIntoDbUser = "SELECT * FROM `users` WHERE `login` LIKE '%$containStrLogin%'"; // запрос к БД
+        $resultAllUsers = requireDB($pathDb,$selectIntoDbAllU); // Выборка всех пользователей
+        $resultUser = requireDB($pathDb,$selectIntoDbUser); // Выборка схожего пользователя
+        $results = requireDBLot($resultAllUsers); // Заносим в мвссив
+        if ($resultUser->num_rows) { // проверка на Логин
+            for ($i = 0; $i < count($results); $i++) { //перебор массива
+                if ($results[$i]->login === strtolower($_POST['userLogin'])) { // проверка логина
+                    if (password_verify($password, $results[$i]->password)) { // сравнение пароля с хеш
+                        $_SESSION['id'] = $results[$i]->user_id; // заносим ID в сессию
+                        echo "<h1>" . "Welcome " . $_POST['userLogin'] . "</h1>";
+                        exit();
+                    } else {
+                        echo "Password not correct!" . "</br>";
+                        echo "Enter password again!" . "</br>";
+                    }
+                }
+            }
+        } else {
+            echo "Login not exist!" . "</br>";
+            echo "Enter login again or sign in!" . "</br>";
+        }
+        closeDB($pathDb); // Закрываем соединение
+
+    } else {
+        echo '<div>' . array_shift($errors) . '</div>';
+    }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
